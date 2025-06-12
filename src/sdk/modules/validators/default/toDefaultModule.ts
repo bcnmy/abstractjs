a7ba019a9103e65eb142632b6eb74eb1284b7291
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-06-12 15:01:46 +0300

    chore: process

--- src/sdk/modules/validators/default/toDefaultModule.ts
+++ src/sdk/modules/validators/default/toDefaultModule.ts
@@ -1,4 +1,4 @@
-import { zeroAddress } from "viem"
+import { type Hex, zeroAddress } from "viem"
 import { DUMMY_SIGNATURE } from "../smartSessions"
 import {
   type Validator,
@@ -7,9 +7,13 @@ import {
 } from "../toValidator"
 
 export const toDefaultModule = (
-  parameters: Omit<ValidatorParameters, "module" | "initData">
-): Validator =>
-  toValidator({
+  parameters: Omit<ValidatorParameters, "module" | "initData"> & {
+    mode?: "simple" | "no_mee" | "permit" | "on-chain"
+    superTxEntriesCount?: number
+  }
+): Validator => {
+  const { mode = "simple", superTxEntriesCount = 3 } = parameters
+  return toValidator({
     initData: parameters.signer.address,
     data: parameters.signer.address,
     deInitData: "0x",
@@ -18,5 +22,23 @@ export const toDefaultModule = (
     module: zeroAddress,
     type: "validator",
     // TODO: make this signature dependent on the mode and numbers of userOps => proof size
-    getStubSignature: async () => DUMMY_SIGNATURE
+    getStubSignature: async (): Promise<Hex> => {
+      return getMeeK1ModuleStubSignature(mode, superTxEntriesCount)
+    }
   })
+}
+
+export const getMeeK1ModuleStubSignature = (
+  mode: "simple" | "no_mee" | "permit" | "on-chain",
+  superTxEntriesCount: number
+): Hex => {
+  // get the proof size for a given merkle tree size
+  const leafCount = superTxEntriesCount + 1
+  const proofSize = Math.ceil(Math.log2(leafCount))
+
+  const proofPayload = "0x"
+
+  const signature = proofPayload + DUMMY_SIGNATURE
+
+  return signature as `0x${string}`
+}
