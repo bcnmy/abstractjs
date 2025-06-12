3e68522dda58e55ef0edfd02a4cd67e69857a804
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-06-12 15:19:59 +0300

    chore: fix case

--- src/sdk/clients/decorators/mee/getQuote.test.ts
+++ src/sdk/clients/decorators/mee/getQuote.test.ts
@@ -388,7 +388,9 @@ describe("mee.getQuote", () => {
     expect(quote.paymentInfo.chainId).to.eq(
       String(DEFAULT_MEE_SPONSORSHIP_CHAIN_ID)
     )
-    expect(quote.paymentInfo.token).to.eq(DEFAULT_MEE_SPONSORSHIP_TOKEN_ADDRESS)
+    expect(quote.paymentInfo.token.toLowerCase()).to.eq(
+      DEFAULT_MEE_SPONSORSHIP_TOKEN_ADDRESS
+    )
 
     expect(quote.userOps[0].userOp.sender).to.eq(
       DEFAULT_MEE_SPONSORSHIP_PAYMASTER_ACCOUNT
