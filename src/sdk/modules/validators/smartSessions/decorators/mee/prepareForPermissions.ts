6fb007aa4f748d8d802885e9178d70387801c398
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-06-23 18:01:26 +0300

    chore: fix build

--- src/sdk/modules/validators/smartSessions/decorators/mee/prepareForPermissions.ts
+++ src/sdk/modules/validators/smartSessions/decorators/mee/prepareForPermissions.ts
@@ -55,7 +55,7 @@ export const prepareForPermissions = async (
 
       const isModuleInstalled_ = (await deployment.isDeployed())
         ? await isModuleInstalled(
-            deployment.client as Client<
+            deployment.client as unknown as Client<
               Transport,
               Chain | undefined,
               ModularSmartAccount
