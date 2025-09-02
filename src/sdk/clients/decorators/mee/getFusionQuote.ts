23fefddb36697125ba93f2b1aa0f94a01e5ded3c
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-09-02 21:43:47 +0300

    chore: fix typo

--- src/sdk/clients/decorators/mee/getFusionQuote.ts
+++ src/sdk/clients/decorators/mee/getFusionQuote.ts
@@ -78,7 +78,7 @@ export type GetFusionQuoteParams = GetQuoteParams & {
  *   walletProvider: "metamask",
  *   trigger: {
  *     chainId: "1",
- *     token: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" // USDC
+ *     tokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" // USDC
  *   },
  *   instructions: [{
  *     to: "0x742d35Cc6634C0532925a3b844Bc454e4438f44e",
