ba944670561cfc72ef36ec6d775b05a0de0f58be
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-12-09 15:43:45 +0300

    chore: new vanity addresses for 2.2.1

--- src/sdk/constants/index.ts
+++ src/sdk/constants/index.ts
@@ -62,10 +62,10 @@ export const DEFAULT_CONFIGURATIONS_BY_MEE_VERSION: Record<
   [MEEVersion.V2_2_1]: {
     version: MEEVersion.V2_2_1,
     accountId: "biconomy.nexus.1.3.1",
-    factoryAddress: "0x74F5a8B5ecf7D634A3dFB5d6bfae4c24635ec921", // Nexus Account Factory Address
-    bootStrapAddress: "0x5d44FD59576404341b07F740e32107ba68640248",
-    implementationAddress: "0x2Dc189b70F8AA151aa1c42D1c97544a206a0bB89", // Nexus 1.3.1
-    validatorAddress: "0x6C6123C7a927895798B4db90e82b3F96da9D834E", // MEE K1 Validator Address
+    factoryAddress: "0x000000002c9A405a196f2dc766F2476B731693c3", // Nexus Account Factory Address
+    bootStrapAddress: "0x000000007BfEdA33ac982cb38eAaEf5D7bCC954c",
+    implementationAddress: "0x0000000020fe2F30453074aD916eDeB653eC7E9D", // Nexus 1.3.1
+    validatorAddress: "0x0000000002d3cC5642A748B6783F32C032616E03", // MEE K1 Validator Address
     defaultValidatorAddress: zeroAddress,
     ethForwarderAddress: "0x000000C48Cdf2b46bEc062483dBD27046dfE3b8d",
     composabilityVersion: ComposabilityVersion.V1_1_0
