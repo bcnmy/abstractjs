56b3720bbe77c39178c477f6740a4b7a44d99ffb
Venkatesh <rvenki666@gmail.com>
2025-12-22 15:08:33 +0530

    chore: build fix

--- src/sdk/clients/decorators/mee/waitForSupertransactionReceipt.ts
+++ src/sdk/clients/decorators/mee/waitForSupertransactionReceipt.ts
@@ -1,5 +1,4 @@
 import type { Hex } from "viem"
-import { parseTransactionStatus } from "../../../account/utils/parseTransactionStatus"
 import type { BaseMeeClient } from "../../createMeeClient"
 import getSupertransactionReceipt, {
   type GetSupertransactionReceiptParams,
